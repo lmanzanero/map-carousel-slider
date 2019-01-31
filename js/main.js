c5926a67bd83cb1aa512316528731329343e38fc
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-01-31 00:23:01 -0600

    removed magnfier functionality and added modal to open slider

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//  MODAL
+
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+    console.log("closing modal");
+    
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
