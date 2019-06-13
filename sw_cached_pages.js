6237351bff6536e539262fb6b74f9921246265f2
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-06-12 22:18:35 -0600

    added images to cacheAssest array for testing

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,8 +1,32 @@
 const cacheName = 'v1';
 
+const cacheAssets = [
+  '/',
+  'index.html', 
+  '/css/style.css',
+  '/js/main.js',
+  'assets/caves_branch_final_2/Pool/Pool-1512122_CO_019(1).jpg', 
+  'assets/caves_branch_final_2/Pool/Pool-cbjl_dh-1021(1).jpg',
+  'assets/caves_branch_final_2/Cheese House/Caves Branch-1.jpg', 
+  'assets/caves_branch_final_2/Cheese House/CheeseCavesBranch2014_85(1).jpg',
+  'assets/caves_branch_final_2/Botanical/Botanical-Botanical 1_ch(1).jpg', 
+  'assets/caves_branch_final_2/Botanical/Botanical-Botanical 2(1).jpg', 
+  'assets/caves_branch_final_2/Botanical/Botanical-P1160083(1).jpg'
+];
+
 // Call Install Event
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
 // Call Activate Event
@@ -26,18 +50,5 @@ self.addEventListener('activate', e => {
 // Call Fetch Event
 self.addEventListener('fetch', e => {
   console.log('Service Worker: Fetching');
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // Make copy/clone of response
-        const resClone = res.clone();
-        // Open cahce
-        caches.open(cacheName).then(cache => {
-          // Add response to cache
-          cache.put(e.request, resClone);
-        });
-        return res;
-      })
-      .catch(err => caches.match(e.request).then(res => res))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
